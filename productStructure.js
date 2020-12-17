3ed0dac570ad72d494cc698af19fa133255dafa0
Felipe Duarte de Lima <39868707+felipeduartedelima@users.noreply.github.com>
2020-12-17 08:00:21 -0300

    Problem resolution

--- productStructure.js
+++ productStructure.js
@@ -14,5 +14,20 @@ const products = [
 ]
 
 module.exports = () => {
-  // Código aqui
+  const numbers = {
+    "PP": 1,
+    "P": 1,
+    "M": 1,
+    "G": 1,
+    "GG": 2,
+    "XG": 3
+  }
+
+  const rightFormat = products.reduce((acc, cur) => {
+    const [color, size] = cur.split("-")
+    acc[color] = { ...acc[color], [size]: numbers[size] }
+
+    return acc;
+  }, {})
+  return rightFormat;
 }
